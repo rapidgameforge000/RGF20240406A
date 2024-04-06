949e2b011bc1fadb023785968d79094a12ce237c
RGF004 <tcagame@tca.ac.jp>
2024-04-06 14:11:37 +0900

    キャラクタの移動を実装

--- Assets/Script/MainCharacter/MainCharacter.cs
+++ Assets/Script/MainCharacter/MainCharacter.cs
@@ -1,13 +1,27 @@
 using Assets.Script.Character;
+using Assets.Script.Collision;
 using UnityEngine;
 
 namespace Assets.Script.MainCharacter
 {
-    public class MainCharacter : MonoBehaviour, ICharacter 
+    public class MainCharacter : MonoBehaviour, ICharacter
     {
+        [SerializeField]
+        private int MOVE_VELOCITY = 50;
+        [SerializeField]
+        private int JUMP_VELOCITY = 80;
+        [SerializeField]
+        private int BRAKE_VELOCITY = 5;
+        [SerializeField]
+        private int MAX_VELOCITY = 50;
+
+
         private RectTransform rectTransform = null;
+        private ICollisionChecker collisionChecker = null;
 
         private Vector2 velocity = Vector2.zero;
+        private int jumpCount = 0;
+        private int maxJumpCount = 10;
 
         private void Awake()
         {
@@ -20,10 +34,9 @@ namespace Assets.Script.MainCharacter
 
         }
 
-        // Update is called once per frame
-        private void Update()
+        internal void Initialize(ICollisionChecker collisionChecker)
         {
-            //Process(0);
+            this.collisionChecker = collisionChecker;
         }
 
         public void Process(float deltaTime)
@@ -34,9 +47,56 @@ namespace Assets.Script.MainCharacter
 
         private void Move(float deltaTime)
         {
-            if (Input.GetKey(KeyCode.Escape))
+            // キー入力.
+            if (Input.GetKey(KeyCode.RightArrow))
             {
+                this.velocity.x += MOVE_VELOCITY * deltaTime;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.velocity.x -= MOVE_VELOCITY * deltaTime;
+            }
+            if (Input.GetKey(KeyCode.Space) && jumpCount < maxJumpCount)
+            {
+                this.velocity.y += JUMP_VELOCITY * deltaTime;
+                ++this.jumpCount;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                this.jumpCount = 0;
+            }
 
+            // ブレーキ.
+            if (System.Math.Abs(this.velocity.x) > (BRAKE_VELOCITY * deltaTime))
+            {
+                this.velocity.x -= BRAKE_VELOCITY * deltaTime * System.Math.Sign(this.velocity.x);
+            }
+            else
+            {
+                this.velocity.x = 0;
+            }
+
+            // 重力
+            if (this.rectTransform.position.y > 0)
+            {
+                this.velocity.y -= (MOVE_VELOCITY * deltaTime) / 2;
+            }
+            else
+            {
+                this.velocity.y = 0;
+            }
+
+            if (this.velocity.sqrMagnitude > (MAX_VELOCITY * MAX_VELOCITY))
+            {
+                this.velocity = this.velocity.normalized * MAX_VELOCITY;
+            }
+
+            this.rectTransform.position += new Vector3(this.velocity.x, this.velocity.y);
+            if (this.rectTransform.position.y < 0)
+            {
+                var pos = this.rectTransform.position;
+                pos.y = 0;
+                this.rectTransform.position = pos;
             }
         }
 
