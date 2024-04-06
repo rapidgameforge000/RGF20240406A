2f6611f48d56391b8fb9846dc5df285a4731b33b
RGF002 <tcagame@tca.ac.jp>
2024-04-06 12:33:52 +0900

    当たり判定用クラス

---
+++ Assets/Script/Collision/ICollision.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script.Collision
+{
+    internal interface ICollision
+    {
+        UnityEngine.Vector2 GetPosition();
+        UnityEngine.Rect GetRect();
+    }
+}
