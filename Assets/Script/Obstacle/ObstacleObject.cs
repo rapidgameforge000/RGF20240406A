d06318f9da5acaec5faa1936241282f665b269c4
RGF001 <tcagame@tca.ac.jp>
2024-04-06 12:35:57 +0900

    rgf001

---
+++ Assets/Script/Obstacle/ObstacleObject.cs
@@ -0,0 +1,17 @@
+namespace Script.Obstacle
+{
+    internal class ObstacleObject : UnityEngine.MonoBehaviour
+    {
+        private UnityEngine.RectTransform _rect_transform;
+        internal void initialize( UnityEngine.RectTransform rect_transform )
+        {
+            
+        }
+
+        internal void update()
+        {
+            
+        }
+
+    }
+}
