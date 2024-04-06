ab83065e9c8866488006f097b9fa0a966d3ddbf7
RGF002 <tcagame@tca.ac.jp>
2024-04-06 12:48:29 +0900

    ICollision修正

--- Assets/Script/SampleScene.cs
+++ Assets/Script/SampleScene.cs
@@ -1,18 +1,21 @@
+using Assets.Script.Stage;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SampleScene : MonoBehaviour
 {
+    private StageManager stageManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.stageManager = new StageManager();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var deltaTime = UnityEngine.Time.deltaTime;
+        this.stageManager.Process(deltaTime);
     }
 }
---
+++ Assets/Script/Stage/StageManager.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Script.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script.Stage
+{
+    internal class StageManager
+    {
+        List<ICollision> enemyList = new List<ICollision>();
+        List<ICollision> obstacleList = new List<ICollision>();
+
+        internal void Process(float deltaTime)
+        {
+            foreach (var enemy in this.enemyList)
+            {
+            }
+        }
+
+        UnityEngine.Vector2 CheckPosition(ICollision collision)
+        {
+            collision.getp
+        }
+
+    }
+}
